4fafd6bae8685c2d3bdc534a731346127f5eefb7
Kajal Maurya <563kajalmaurya@gmail.com>
2026-04-03 03:12:50 +0530

    First commit - Ai-interviewer

---
+++ Backend/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config()
+const app = require("./src/app")
+const connectToDB = require("./src/config/database")
+const invokeGeminiAi = require("./src/services/ai.service")
+
+
+connectToDB()
+invokeGeminiAi()
+app.listen(3000, () =>{
+    console.log("Server is running on port 3000")
+})
+
+
+// async function startServer() {
+//     await connectToDB(); // wait for DB
+
+//     app.listen(3000, () => {
+//         console.log("Server is running on port 3000");
+//     });
+// }
+
+// startServer();
